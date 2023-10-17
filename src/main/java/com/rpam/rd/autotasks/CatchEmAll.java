742c08c752a0f1f9f530211ad81490ddef482d7b
Ekaterina Avdeenko <avdeenqoeka@gmail.com>
2023-10-17 17:41:29 +0000

    Update CatchEmAll.java

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,17 +1,40 @@
 package com.rpam.rd.autotasks;
 
+import java.io.IOException;
 import java.io.FileNotFoundException;
-
 public class CatchEmAll {
+  static Exception exception = null;
 
-    //You may set another exception in this field;
-    static Exception exception = new FileNotFoundException();
-
-    public static void riskyMethod() throws Exception {
-        throw exception;
+    private static void riskyMethod() throws Exception {
+        if (exception != null) {
+            throw exception;
+        }
     }
-
     public static void main(String[] args) throws Exception {
-        riskyMethod();
-    }
-}
+
+       if (args.length >= 1) {
+           if (args[0].equals("IOException")) {
+             exception = new IOException("IO exception");
+          } else if (args[0].equals("FileNotFoundException")) {
+              exception = new FileNotFoundException("file not found exception");
+           } else if (args[0].equals("ArithmeticException")) {
+               exception = new ArithmeticException("arithmetic exception");
+           } else if (args[0].equals("NumberFormatException")) {
+                exception = new NumberFormatException("");
+            } else if (args[0].equals("Exception")) {
+                exception = new Exception("exception");
+           } else {
+               System.out.println("Используйте аргумент IOException, FileNotFoundException, ArithmeticException, NumberFormatException, Exception");
+           }}
+
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("Resource is missing", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Resource is error", e);
+        } catch (ArithmeticException|NumberFormatException e) {
+            System.err.print(e);
+        }}}
+
+
